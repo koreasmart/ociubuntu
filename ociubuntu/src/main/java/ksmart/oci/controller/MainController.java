8b9c725a3f3fe5485063b2bff04d8930ce839a4f
coolhongs <coolhongs@naver.com>
2023-11-27 12:54:40 +0900

    커

--- ociubuntu/src/main/java/ksmart/oci/controller/MainController.java
+++ ociubuntu/src/main/java/ksmart/oci/controller/MainController.java
@@ -1,14 +1,22 @@
 package ksmart.oci.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @Controller
 public class MainController {
 
+	
+	private static final Logger log = LoggerFactory.getLogger(MainController.class);
+
 	@GetMapping("/")
-	public String main(Model model) {
+	public String main(Model model, HttpServletRequest request) {
+		log.info(request.getRemoteAddr());
 		return "main";
 	}
 }
